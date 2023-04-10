2c2de83e0bac98ef7d19e300e91bfbfc49c39477
HYnllvo <gpalsu0812@naver.com>
2023-04-11 04:15:32 +0900

    db/tweet model, admin 수정

--- tweet/models.py
+++ tweet/models.py
@@ -5,15 +5,21 @@ from user.models import UserModel
 
 class TweetModel(models.Model):
 
-    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    image = models.URLField('url', unique=True, blank=True)  # 사진이 없어도 트윗 추가 가능
-    content = models.TextField(verbose_name="내용", null=False)
+    user = models.ForeignKey(
+        UserModel, on_delete=models.CASCADE, verbose_name="이름")
+    image = models.URLField('이미지', unique=True, blank=True,
+                            null=True)  # 사진이 없어도 트윗 추가 가능
+    content = models.TextField("내용")
+    update_at = models.DateTimeField("수정날짜", auto_now=True)
     # ordering(피드정렬)='-created_at'
-    update_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateField("생성날짜", auto_now_add=True)
 
     class Meta:     # 데이터 베이스에 정보를 넣어주는 역할
         db_table = "tweet"  # 테이블 이름
 
+    @property   # 메소드의 return 값을 필드로 정의
+    def short_content(self):
+        return self.content[:15]    # title 대신 short_content로 간단하게 admin에서 보여주기
+
     def __str__(self):
-        return str(self.id)
+        return self.short_content
