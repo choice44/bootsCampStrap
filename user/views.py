f872b0bedd53190a7680b74540bccb3b3f3464f6
wooseok <william741@naver.com>
2023-04-11 09:19:23 +0900

    for new pull

--- user/views.py
+++ user/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from.forms import SignUpForm
 
-# Create your views here.
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'user/signup.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        password2 = request.POST.get('password2', '')
+
+        if password != password2:
+            
