c8cbaa0e962297559a12006754649b14d4db6e5b
wooseok <william741@naver.com>
2023-04-11 11:46:37 +0900

    Fix the wrong upbranch

--- user/models.py
+++ user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# class UserModel(models.Model):
+#     class Meta:
+#         db_table = "user"
+
+#     username = models.CharField("이름", max_length=50)
+#     password = models.CharField("비밀번호", max_length=500)
+#     bio = models.TextField("자기소개", blank=True)
+
+#     def __str__(self):
+#         return self.username
+
+class UserModel(AbstractUser):
+    class Meta:
+        db_table = "user"
+
+    bio = models.TextField(max_length=500, default='')
--- user/views.py
+++ user/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import UserModel
+from django.contrib.auth import get_user_model
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'user/signup.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        password2 = request.POST.get('password2', '')
+
+        if password != password2:
+            return render(request, 'user/signup.html', {'error': '비밀번호를 확인 해 주세요!'})
+        else:
+            if username == '' or password == '':
+                return render(request, 'user/signup.html', {'error': '아이디와 비밀번호는 필수 값 입니다.'})
+
+            exist_user = get_user_model().objects.filter(username=username)
+            if exist_user:
+                return render(request, 'user/signup.html', {'error': '아이디가 이미 존재합니다.'})
+            else:
+                UserModel.objects.create_user(username=username, password=password, bio='')
+                return redirect('/login')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+
+        me = auth.authenticate(request, username=username, password=password)
+        if me is not None:
+            auth.login(request, me)
+            return redirect('/')
+        else:
+            return render(request, 'user/login.html', {'error': '아이디 혹은 비밀번호를 확인 해 주세요'})
+
+    elif request.method == 'GET':
+        return render(request, 'user/login.html')            
+
+
+@login_required
+def logout(request):
+    auth.logout(request)    # 인증 되어있는 정보를 없애기
+    return redirect('/')
