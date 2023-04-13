9a977a86cc48e28988c97487422bae0b3cbaaa48
wooseok <william741@naver.com>
2023-04-13 11:01:38 +0900

    팔로우 게시물 페이지 구현

--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import UserModel
 from .forms import UserForm, EditProfileForm
 from tweet.views import my_page
+from tweet.models import TweetModel
 from django.contrib.auth import get_user_model, authenticate, login
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
@@ -57,3 +58,13 @@ def follow_function(request, user_id):
             follow_user.save()
 
     return HttpResponseRedirect(reverse(my_page, args=[user_id]))
+
+
+@login_required
+def follow_tweet_view(request):
+    login_user = request.user
+    follow_users = login_user.follower.all()
+    follow_users_ids = [user.id for user in follow_users]
+    follow_tweet = TweetModel.objects.filter(user_id__in=follow_users_ids)
+    all_follow_tweet = follow_tweet.order_by('-created_at')
+    return render(request, 'tweet/follows_tweet.html', {'all_follow_tweet': all_follow_tweet})
