63740de790cb73fa315078501b358820000694f4
Choi Seungwon <cso6549@naver.com>
2023-04-14 15:45:16 +0900

    :fire: user/models.py/imgfile -null=True, :sparkles:button: on editprofile page

--- user/models.py
+++ user/models.py
@@ -12,7 +12,7 @@ class UserModel(AbstractUser):
     bio = models.TextField("자기소개", blank=True)
     # blank를 지정하지 않으면, 이미지 값을 넣어달라고 요청함
     # null 이미지를 넣지 않아도 오류가 안남
-    imgfile = models.ImageField(null=True, upload_to="user_images", blank=True, default="static/image/defaultprofileimage.jpeg")
+    imgfile = models.ImageField(upload_to="user_images", blank=True, default="static/image/defaultprofileimage.jpeg")
 
     def __str__(self):
         return self.username
